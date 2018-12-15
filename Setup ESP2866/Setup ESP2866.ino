dab93f71c5309fac45a07f009f1ad0a1b2a3159c
florian gabriel <flow@p0cki.net>
2018-12-15 18:57:37 +0100

    Projektdateien hinzufügen.

---
+++ Setup ESP2866/Setup ESP2866.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Setup_ESP2866.ino
+ Created:	15.12.2018 18:56:19
+ Author:	flow
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
